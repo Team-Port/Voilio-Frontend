34fe7388a3a7a6a7a669de3d315ea3b1e2e4ea7c
gilyeon00 <12venus08@naver.com>
2023-03-21 13:33:07 +0900

    [#1]Design: Home page 구조설계

---
+++ src/page/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='home-wrap'>
+            <div className='left-sidebar-box'>
+                <div className='page-menu-box'>
+
+                </div>
+                <div className='page-menu-box'>
+
+                </div>
+            </div>
+            <div className='video-list'>
+                
+            </div>
+            <div className='right-sidebar-box'>
+
+            </div>
+        </div>
+    );
+};
+
+export default Home;
