7dc90cfde65adca5cc194c7cd13c03d972faf3ea
gilyeon00 <12venus08@naver.com>
2023-03-21 15:26:08 +0900

    [#1]Design: Home page - Header 퍼블리싱 완료

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './component/Header';
 import './App.css';
+import Home from './page/Home';
 
 function App() {
+    return(
+        <BrowserRouter>
+            <Header>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                </Routes>
+            </Header>
+        
+        </BrowserRouter>
+
+    );
   
 }
 
