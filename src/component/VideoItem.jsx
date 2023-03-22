38ce69d87f7bd4deb874d6adf78f668faa7740a6
gilyeon00 <12venus08@naver.com>
2023-03-22 09:49:58 +0900

    [#1]Feat: VideoItem 추가

--- src/component/VideoItem.jsx
+++ src/component/VideoItem.jsx
@@ -3,9 +3,68 @@ import './videoItem.css'
 
 const VideoItem = () => {
     return (
-        <div>
-            
-        </div>
+        <li className='videoItem'>
+            <div className='item'>
+                <div className='thumnail'>
+                    <img src={'asset/test_img.jpeg'} alt="videoThumnail" />
+                </div>
+                <div className='metaData'>
+                    <div className='channelImg'>
+                        <img src ={'asset/test_thum.jpeg'} alt="channelImg-thum"/>
+                    </div>
+                    <div className='infoText'>
+                        <p className='title'>video Title</p>
+                        <p className='channel'>channel Name</p>
+                        <p className='publishDate'>published Date</p>
+                    </div>
+                </div>
+            </div>
+            <div className='item'>
+                <div className='thumnail'>
+                    <img src={'asset/test_img.jpeg'} alt="videoThumnail" />
+                </div>
+                <div className='metaData'>
+                    <div className='channelImg'>
+                        <img src ={'asset/test_thum.jpeg'} alt="channelImg-thum"/>
+                    </div>
+                    <div className='infoText'>
+                        <p className='title'>video Title</p>
+                        <p className='channel'>channel Name</p>
+                        <p className='publishDate'>published Date</p>
+                    </div>
+                </div>
+            </div>
+            <div className='item'>
+                <div className='thumnail'>
+                    <img src={'asset/test_img.jpeg'} alt="videoThumnail" />
+                </div>
+                <div className='metaData'>
+                    <div className='channelImg'>
+                        <img src ={'asset/test_thum.jpeg'} alt="channelImg-thum"/>
+                    </div>
+                    <div className='infoText'>
+                        <p className='title'>video Title</p>
+                        <p className='channel'>channel Name</p>
+                        <p className='publishDate'>published Date</p>
+                    </div>
+                </div>
+            </div>
+            <div className='item'>
+                <div className='thumnail'>
+                    <img src={'asset/test_img.jpeg'} alt="videoThumnail" />
+                </div>
+                <div className='metaData'>
+                    <div className='channelImg'>
+                        <img src ={'asset/test_thum.jpeg'} alt="channelImg-thum"/>
+                    </div>
+                    <div className='infoText'>
+                        <p className='title'>video Title</p>
+                        <p className='channel'>channel Name</p>
+                        <p className='publishDate'>published Date</p>
+                    </div>
+                </div>
+            </div>
+        </li>
     );
 };
 
