ef86b9b016137e68fb4872bff2250f72a5deef3d
Kyongmin1016 <rkdrnwkd1@gmail.com>
2023-05-07 21:30:34 +0900

    Feat: 게시물 삭제 API 연동

--- src/component/VideoItem.jsx
+++ src/component/VideoItem.jsx
@@ -1,11 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
 import "./videoItem.css";
 import { Link } from "react-router-dom";
 import * as common from "./../lib/common";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { Box, IconButton, Menu, MenuItem } from "@mui/material";
+import axios from "axios";
+import { HOST_URL } from "../lib/HostUrl";
 
-const VideoItem = ({ videoItem }) => {
+const ITEM_HEIGHT = 48;
+const options = ["숨김", "삭제"];
+
+const VideoItem = ({ videoItem, auth }) => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const clickOption = (option, boardId) => {
+    if (option.target.innerText === "삭제") {
+      axios.delete(`${HOST_URL}/api/v1/boards/${boardId}`).then((respone) => {
+        if (respone.status === 200) {
+          window.location.reload();
+        }
+      });
+    } else {
+      console.log("숨김");
+    }
+  };
+
   return (
     <li className={`videoItem`}>
+      {auth ? (
+        <div>
+          <IconButton
+            aria-label="more"
+            id="long-button"
+            aria-controls={open ? "long-menu" : undefined}
+            aria-expanded={open ? "true" : undefined}
+            aria-haspopup="true"
+            onClick={handleClick}
+          >
+            <MoreVertIcon />
+          </IconButton>
+          <Menu
+            id="long-menu"
+            MenuListProps={{
+              "aria-labelledby": "long-button",
+            }}
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            PaperProps={{
+              style: {
+                maxHeight: ITEM_HEIGHT * 4.5,
+                width: "20ch",
+              },
+            }}
+          >
+            {options.map((option) => (
+              <MenuItem
+                key={option}
+                selected={option === "Pyxis"}
+                onClick={(option) => {
+                  clickOption(option, videoItem.id);
+                }}
+                valu
+              >
+                {option}
+              </MenuItem>
+            ))}
+          </Menu>
+        </div>
+      ) : (
+        <></>
+      )}
       <Link to={`/watch/${videoItem.id}`}>
         <div className="item">
           <div className="thumnail">
--- src/component/VideoList.jsx
+++ src/component/VideoList.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import "./videoList.css";
 import VideoItem from "./VideoItem";
 
-const VideoList = ({ videoItems }) => {
+const VideoList = ({ videoItems, auth }) => {
   return (
     <ul className="videoList">
       {
         // videoItem이 불러와지면 화면에 뿌린다.
         videoItems &&
           Object.values(videoItems).map((videoItem) => (
-            <VideoItem videoItem={videoItem} key={videoItem.id} />
+            <VideoItem videoItem={videoItem} key={videoItem.id} auth={auth} />
           ))
       }
     </ul>
