4ded439c7491501960b59e28c6a01cb16c0eb435
Xoeon <jayy_19@ewhain.net>
2023-10-08 21:17:42 +0900

    [#42]Chore: end point 수정

--- src/component/NewMemberBox.jsx
+++ src/component/NewMemberBox.jsx
@@ -1,50 +1,56 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { HOST_URL } from "../lib/HostUrl";
-import './infoList.css'
+import "./infoList.css";
 import { useNavigate } from "react-router-dom";
 
 const NewMemberBox = () => {
-    const [newMembers, setNewMembers] = useState([]);
-    const navigate = useNavigate();
+  const [newMembers, setNewMembers] = useState([]);
+  const navigate = useNavigate();
 
-    const getNewMembers = () => {
-        axios
-          .get(`${HOST_URL}/api/v1/users/showAll`)
-          .then((response) => {
-            if (response.data.status === "200") {
-              setNewMembers(response.data.data);
-            }
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      };
+  const getNewMembers = () => {
+    axios
+      .get(`${HOST_URL}/api/v1/users/list`)
+      .then((response) => {
+        if (response.data.status === "200") {
+          setNewMembers(response.data.data);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-    useEffect(() => {
-        getNewMembers();
-    }, []);
+  useEffect(() => {
+    getNewMembers();
+  }, []);
 
-    const moveToUser = (nickname) => {
-        navigate(`/profile/@${nickname}`);
-    };
+  const moveToUser = (nickname) => {
+    navigate(`/profile/@${nickname}`);
+  };
 
-    return (
-        <div className='infoMenu-box'>
-            <div className='subscribe-title'>
-                <h3>❣️ NEW Members ❣️</h3>
-            </div>
-            <div className='subscribe-box'>
-                {newMembers.map((member, index) => (
-                    <div key={index} onClick={() => moveToUser(member.nickname)}>
-                        <img className='subscribeNum' src={process.env.PUBLIC_URL + `/asset/num${index + 1}.png`} />
-                        <img className='ss-channelImg' src={process.env.PUBLIC_URL + '/asset/test_thum.jpeg'} />
-                        {member.nickname}
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div className="infoMenu-box">
+      <div className="subscribe-title">
+        <h3>❣️ NEW Members ❣️</h3>
+      </div>
+      <div className="subscribe-box">
+        {newMembers.map((member, index) => (
+          <div key={index} onClick={() => moveToUser(member.nickname)}>
+            <img
+              className="subscribeNum"
+              src={process.env.PUBLIC_URL + `/asset/num${index + 1}.png`}
+            />
+            <img
+              className="ss-channelImg"
+              src={process.env.PUBLIC_URL + "/asset/test_thum.jpeg"}
+            />
+            {member.nickname}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default NewMemberBox;
