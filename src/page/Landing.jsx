9860ff125c6f15d4057ca2f13c07a8448cb0f16a
miyoung12 <miseja3@gmail.com>
2023-10-13 03:03:21 +0900

    feat: 랜딩페이지 수정

--- src/page/Landing.jsx
+++ src/page/Landing.jsx
@@ -3,7 +3,12 @@ import "../../src/styles/tailwind.css";
 
 const Landing = () => {
   return (
-    <div className="bg-white relative z-0">
+    <div className="flex flex-col bg-white relative z-0">
+      <div className="z-10 flex justify-between text-neutral-500 text-[40px] font-almendra font-bold top-10 left-8 absolute w-[95%]">
+        <div className="">video</div>
+        <div className="">portfolio</div>
+        <div className="">service</div>
+      </div>
       <div className="flex justify-center items-center">
         <img
           src="/asset/gradient.png"
@@ -13,37 +18,38 @@ const Landing = () => {
         <img
           src="/asset/landing.png"
           alt="landing"
-          className="z-10 w-[78%] h-auto absolute"
+          className="z-10 w-[75%] h-auto absolute"
         />
         <img
           src="/asset/arrow.png"
           alt="arrow"
-          className="z-20 w-[30%] h-auto absolute m-0 left-0 top"
+          className="z-20 w-[27%] h-auto absolute m-0 left-0 top"
         />
         <img
           src="/asset/start.png"
+          ㅊ
           alt="start service"
-          className="z-20 w-[15%] h-auto absolute bottom-10"
+          className="z-20 w-[10%] h-auto absolute bottom-24"
         />
         <img
           src="/asset/downarrow.png"
           alt="downarrow"
-          className="z-20 w-[8%] h-auto absolute bottom-20"
+          className="z-20 w-[5%] h-auto absolute bottom-32"
         />
         <img
           src="/asset/clip.png"
           alt="clip"
-          className="z-20 w-[7%] h-auto absolute right-20 top-30"
+          className="z-20 w-[7%] h-auto absolute right-36 bottom-80"
         />
         <img
           src="/asset/title1.png"
           alt="영상기반 포토폴리오 서비스"
-          className="z-20 w-[30%] h-auto absolute right-20 top"
+          className="z-20 w-[30%] h-auto absolute right-52 bottom-96"
         />
         <img
           src="/asset/title2.png"
           alt="voilio"
-          className="z-20 w-[30%] h-auto absolute right-20 top-80"
+          className="z-20 w-[30%] h-auto absolute right-44 bottom-52"
         />
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        almendra: ["Almendra", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
