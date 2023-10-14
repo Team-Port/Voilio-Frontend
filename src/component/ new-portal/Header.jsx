139e4b2123607801ca04c5e40fdc0e8432691523
Xoeon <jayy_19@ewhain.net>
2023-10-15 01:45:24 +0900

    [#45]Refactor: 주석 삭제

--- src/component/ new-portal/Header.jsx
+++ src/component/ new-portal/Header.jsx
@@ -1,10 +1,6 @@
 import { useLocation } from "react-router-dom";
 
 const Header = () => {
-  // const location = useLocation();
-  // const hideHeader = location.pathname === "/new-portal/login";
-
-  // if (hideHeader) return null;
   return (
     <div className="w-full h-[85px] bg-white flex flex-row">
       <div className="flex flex-row">
