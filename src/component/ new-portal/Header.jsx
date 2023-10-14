bf62996e55b85aca0d9cde057db28bbce08d03d5
Xoeon <jayy_19@ewhain.net>
2023-10-15 01:20:08 +0900

    [#45]Feat: Header component 수정

--- src/component/ new-portal/Header.jsx
+++ src/component/ new-portal/Header.jsx
@@ -6,28 +6,32 @@ const Header = () => {
 
   if (hideHeader) return null;
   return (
-    <div className="bg-white">
-      <div className="relative flex flex-row w-full h-[76px]">
-        <img className="absolute top-[-16px]" src="/asset/bg-header.svg" />
+    <div className="w-full h-[85px] bg-white flex flex-row">
+      <div className="flex flex-row">
         <img
-          className="absolute left-[20px] w-[130px]"
-          src="/asset/new-logo.svg"
-          alt="voilio-logo"
+          className="absolute left-0 m-0"
+          src="/asset/bg-header-rounded.svg"
         />
-        <div className="flex items-center w-auto px-[18px] absolute flex-shrink-0 mt-[10px] left-[210px] rounded-[63px] border-[0.5px] border-[#6E6E6E] h-[55px]">
-          <img className="w-[31px]" src="/asset/Icon_magnifier.svg" />
+        <img
+          className="absolute left-[25px] top-[20px] m-0 w-[150px]"
+          src="/asset/new-logo.svg"
+        />
+      </div>
+      <div className="relative h-full py-[15px] left-[250px] flex-shrink-0">
+        <div className="flex h-full items-center px-[18px] rounded-[63px] border-[0.5px] border-[#6E6E6E]">
+          <img className="w-[25px]" src="/asset/Icon_magnifier.svg" />
           <input
-            className="text-[22px] ml-[11px] w-full outline-none"
+            className="text-[20px] ml-[11px] mr-[150px] w-full outline-none"
             type="text"
             placeholder="Search anything ..."
           />
-          <img className="ml-[270px]" src="/asset/Icon_option.svg" />
+          <img className="" src="/asset/Icon_option.svg" />
         </div>
-        <div className="w-full flex flex-shrink-0 justify-end items-center gap-[25px] pr-[25px]">
-          <img src="/asset/Icon_upload.svg" />
-          <img src="/asset/Icon_chat.svg" />
-          <img src="/asset/Icon_mypage.svg" />
-        </div>
+      </div>
+      <div className="w-full flex flex-row justify-end items-center gap-[25px] pr-[25px]">
+        <img src="/asset/Icon_upload.svg" />
+        <img src="/asset/Icon_chat.svg" />
+        <img src="/asset/Icon_mypage.svg" />
       </div>
     </div>
   );
