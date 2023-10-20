56f3a8e5c5596ce00e7d86faf915a483f292201e
Xoeon <jayy_19@ewhain.net>
2023-10-20 23:47:58 +0900

    [#48]Feat: add React quill

--- src/page/UploadVideoNew.jsx
+++ src/page/UploadVideoNew.jsx
@@ -1,3 +1,62 @@
+import { fullscreen } from "@uiw/react-md-editor";
+import ReactQuill from "react-quill";
+
+const modules = {
+  toolbar: {
+    container: [
+      [{ header: [1, 2, 3, false] }],
+      [{ font: [] }],
+      [{ align: [] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [{ list: "ordered" }, { list: "bullet" }, "link"],
+      [
+        {
+          color: [
+            "#000000",
+            "#e60000",
+            "#ff9900",
+            "#ffff00",
+            "#008a00",
+            "#0066cc",
+            "#9933ff",
+            "#ffffff",
+            "#facccc",
+            "#ffebcc",
+            "#ffffcc",
+            "#cce8cc",
+            "#cce0f5",
+            "#ebd6ff",
+            "#bbbbbb",
+            "#f06666",
+            "#ffc266",
+            "#ffff66",
+            "#66b966",
+            "#66a3e0",
+            "#c285ff",
+            "#888888",
+            "#a10000",
+            "#b26b00",
+            "#b2b200",
+            "#006100",
+            "#0047b2",
+            "#6b24b2",
+            "#444444",
+            "#5c0000",
+            "#663d00",
+            "#666600",
+            "#003700",
+            "#002966",
+            "#3d1466",
+          ],
+        },
+        { background: [] },
+      ],
+      ["image"],
+      ["clean"],
+    ],
+  },
+};
+
 const UploadVideo = () => {
   return (
     <div className="relative flex justify-center flex-1">
@@ -62,10 +121,13 @@ const UploadThumbnail = () => {
 
 const UploadVideoNew = () => {
   return (
-    <div className="pl-[250px] pr-[20px] pt-[150px]">
+    <div className="pl-[250px] pr-[20px] pt-[115px]">
       <div className="flex flex-row gap-[30px]">
         <UploadVideo />
         <UploadThumbnail />
+      </div>
+      <div className="bg-white mx-[70px] mt-[50px] mb-[20px]">
+        <ReactQuill modules={modules} />
       </div>
     </div>
   );
