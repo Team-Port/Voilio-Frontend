77978de2bf2776d510e786482106a4ca28924dfb
Xoeon <jayy_19@ewhain.net>
2023-10-28 00:29:12 +0900

    [#57]Feat: AuthInput 컴포넌트 수정

--- src/component/ new-portal/AuthInput.jsx
+++ src/component/ new-portal/AuthInput.jsx
@@ -1,9 +1,15 @@
-const AuthInput = ({ formValue, placeholder, icon }) => {
+const AuthInput = ({ formTitle, placeholder, icon, value, setValue }) => {
   return (
     <div className="text-[#1E293B] flex flex-col gap-[8px]">
-      <div className="text-sm font-semibold">{formValue}</div>
+      <div className="text-sm font-semibold">{formTitle}</div>
       <div className="flex flex-row border-[1px] border-[#E2E8F0] rounded-[4px] px-[12px] py-[16px] bg-white">
-        <input className="w-full outline-none" placeholder={placeholder} />
+        <input
+          className="w-full outline-none"
+          placeholder={placeholder}
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+        />
         {icon && <img className="m-[0px]" src={icon} />}
       </div>
     </div>
