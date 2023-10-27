9edbc05016d361b090966cbd04f248de01f6d018
Xoeon <jayy_19@ewhain.net>
2023-10-28 00:29:27 +0900

    [#57]Feat: Signin API 연결

--- src/page/new-portal/Signin.jsx
+++ src/page/new-portal/Signin.jsx
@@ -1,4 +1,51 @@
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
+
+import { HOST_URL } from "../../lib/HostUrl";
+
+import AuthInput from "../../component/ new-portal/AuthInput";
+import ServiceIntro from "../../component/ new-portal/ServiceIntro";
+
 const Signin = () => {
+  const [emailValue, setEmailValue] = useState("");
+  const [pwdValue, setPwdValue] = useState("");
+  const [confirmPwdValue, setConfirmPwdValue] = useState("");
+  const [nicknameValue, setNicknameValue] = useState("");
+
+  const navigate = useNavigate();
+
+  const signinAxios = () => {
+    axios
+      .post(`${HOST_URL}/api/v1/auth/signup`, {
+        email: emailValue,
+        password: pwdValue,
+        nickname: nicknameValue,
+      })
+      .then((response) => {
+        console.log(response);
+        alert("회원가입이 완료되었습니다.");
+        if (response.status === 200) {
+          return navigate("/new-portal/login");
+        }
+      })
+      .catch((err) => {
+        console.log(err.request.status);
+        var errcode = err.request.status;
+        if (errcode === 500) {
+          alert("이미 사용중인 이메일입니다.");
+        }
+      });
+  };
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+
+    if (pwdValue !== confirmPwdValue) {
+      return alert("비밀번호가 일치하지 않습니다.");
+    }
+  };
+
   return (
     <div className="relative flex bg-[#F8FAFC] flex-row">
       <img
@@ -8,76 +55,44 @@ const Signin = () => {
       />
       <div className="h-[100vh] px-[12%] w-1/2 items-center justify-center flex">
         <div className="flex flex-col w-full">
-          <div className="text-[#1E293B] font-bold text-4xl mb-[16px]">
-            Welcome Voilio
-          </div>
-          <div className="text-[#475569] mb-[30px]">
-            <li>
-              Voilio는{" "}
-              <span className="text-[#FF6A8C]">
-                영상 기반 포트폴리오 공유 웹사이트
-              </span>
-              예요.
-            </li>
-            <li>로그인을 하면 다양한 영상을 공유해서 나를 알릴 수 있어요.</li>
-            <li>다양한 분야의 사람들, 채용 담당자들과 DM을 해보세요.</li>
-          </div>
-          <div className="flex flex-col gap-[16px] mb-[32px]">
-            <div className="text-[#1E293B] flex flex-col gap-[8px]">
-              <div className="text-sm font-semibold">E-mail</div>
-              <div className="border-[1px] border-[#E2E8F0] rounded-[4px] px-[12px] py-[16px] bg-white">
-                <input
-                  className="w-full outline-none"
-                  placeholder="아이디로 사용할 이메일을 입력해 주세요."
-                />
-              </div>
+          <ServiceIntro />
+          <form onSubmit={onSubmitHandler}>
+            <div className="flex flex-col gap-[16px] mb-[32px]">
+              <AuthInput
+                formTitle="E-mail"
+                placeholder="아이디로 사용할 이메일을 입력해 주세요."
+                value={emailValue}
+                setValue={setEmailValue}
+              />
+              <AuthInput
+                formTitle="Password"
+                placeholder="비밀번호를 입력해 주세요."
+                icon="/asset/Icon_eyeOff.svg"
+                value={pwdValue}
+                setValue={setPwdValue}
+              />
+              <AuthInput
+                formTitle="Password Check"
+                placeholder="비밀번호를 한 번 더 입력해 주세요."
+                icon="/asset/Icon_eyeOff.svg"
+                value={confirmPwdValue}
+                setValue={setConfirmPwdValue}
+              />
+              <AuthInput
+                formTitle="Password"
+                placeholder="채널 이름으로 사용할 닉네임을 입력해 주세요."
+                icon="/asset/Icon_eyeOff.svg"
+                value={nicknameValue}
+                setValue={setNicknameValue}
+              />
             </div>
-            <div className="text-[#1E293B] flex flex-col gap-[8px]">
-              <div className="text-sm font-semibold">Password</div>
-              <div className="flex flex-row border-[1px] border-[#E2E8F0] rounded-[4px] px-[12px] py-[16px] bg-white">
-                <input
-                  className="w-full outline-none"
-                  placeholder="비밀번호를 입력해 주세요."
-                />
-                <img
-                  className="m-[0px]"
-                  src="/asset/Icon_eyeOff.svg"
-                  alt="Icon_eyeOff"
-                />
-              </div>
-            </div>
-            <div className="text-[#1E293B] flex flex-col gap-[8px]">
-              <div className="text-sm font-semibold">Password Check</div>
-              <div className="flex flex-row border-[1px] border-[#E2E8F0] rounded-[4px] px-[12px] py-[16px] bg-white">
-                <input
-                  className="w-full outline-none"
-                  placeholder="비밀번호를 한 번 더 입력해 주세요."
-                />
-                <img
-                  className="m-[0px]"
-                  src="/asset/Icon_eyeOff.svg"
-                  alt="Icon_eveOff"
-                />
-              </div>
-            </div>
-            <div className="text-[#1E293B] flex flex-col gap-[8px]">
-              <div className="text-sm font-semibold">Nickname</div>
-              <div className="flex flex-row border-[1px] border-[#E2E8F0] rounded-[4px] px-[12px] py-[16px] bg-white">
-                <input
-                  className="w-full outline-none"
-                  placeholder="채널 이름으로 사용할 닉네임을 입력해 주세요."
-                />
-                <img
-                  className="m-[0px]"
-                  src="/asset/Icon_random.svg"
-                  alt="Icon_random"
-                />
-              </div>
-            </div>
-          </div>
-          <div className="flex justify-center px-[24px] py-[16px] mb-[29px] bg-[#FACAD5] rounded-[4px] text-white font-bold">
-            Join
-          </div>
+            <button
+              className="flex justify-center w-full px-[24px] py-[16px] mb-[29px] bg-[#FACAD5] rounded-[4px] text-white font-bold"
+              onClick={signinAxios}
+            >
+              Join
+            </button>
+          </form>
         </div>
       </div>
       <img
