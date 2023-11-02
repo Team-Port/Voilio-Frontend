6e2bdc9a56423d9f459225d489144bbba3f46b53
miyoung12 <miseja3@gmail.com>
2023-11-02 23:55:41 +0900

    videoitem api연동 수정 중

--- src/component/ new-portal/VideoList.jsx
+++ src/component/ new-portal/VideoList.jsx
@@ -17,6 +17,7 @@ const VideoList = () => {
         .then((response) => {
           if (response.status === 200) {
             setItems(response.data.data.content);
+            console.log("게시글을 정상적으로 불러왔습니다.");
           }
         })
         .catch((error) => {
@@ -27,15 +28,26 @@ const VideoList = () => {
 
   return (
     <div className="w-full h-full">
-      <div className="grid grid-cols-3 grid-rows-3 gap-4 gap-4 pl-[20px] pr-[55px]">
+      <div className="grid grid-cols-3 grid-rows-3 gap-4 pl-[20px] pr-[70px]">
         {items.map((item) => (
-          <div key={item.id} className="bg-white bg-opacity-75 rounded-[10px]">
-            <VideoItem item={item} /> {/* boardId를 props로 전달합니다. */}
+          <div
+            key={item.id}
+            className="bg-white bg-opacity-75 rounded-[10px] gap-[10px]"
+          >
+            <VideoItem
+              title={item.title}
+              content={item.content}
+              category1={item.category1}
+              category2={item.category2}
+              createDate={item.createDate}
+              imageUrl={item.imageUrl}
+              thumbnailUrl={item.thumbnailUrl}
+            />
+            {/* boardId를 props로 전달합니다. */}
           </div>
         ))}
       </div>
     </div>
-    // <VideoList></VideoList>
   );
 };
 
