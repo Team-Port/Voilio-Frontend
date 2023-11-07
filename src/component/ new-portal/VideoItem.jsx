4dcf6b76b7049fb1ff68306f43249bce8e5e3f82
miyoung12 <96506175+miyoung12@users.noreply.github.com>
2023-11-07 19:00:09 +0900

    Revert "(#70)feat/board api"

--- src/component/ new-portal/VideoItem.jsx
+++ src/component/ new-portal/VideoItem.jsx
@@ -1,33 +1,74 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../../../src/styles/tailwind.css";
-import Category from "../../component/ new-portal/Category";
+import axios from "axios";
+import { HOST_URL } from "../../lib/HostUrl";
 
-const VideoItem = ({
-  title,
-  content,
-  category1,
-  category2,
-  createAt,
-  imageUrl,
-  thumbnailUrl,
-}) => {
-  const createDate = new Date(createAt);
-  const month = createDate.getMonth() + 1; // getMonth()는 0부터 시작하므로 1을 더해줍니다.
-  const day = createDate.getDate();
+const VideoItem = ({ item }) => {
+  const [data, setData] = useState(null);
+  const [title, setTitle] = useState(null);
+  const [content, setContent] = useState(null);
+  const [category1, setCategory1] = useState(null);
+  const [category2, setCategory2] = useState(null);
+  const categoryColors = {
+    IT: "#85AED3",
+    Design: "#FACAD5",
+    Dance: "#EAB191",
+    Exercise: "#A9D8B6",
+    Language: "#CFB8E1",
+    Sales: "#E1DE89",
+  };
+  const category1Color = categoryColors[item.category1] || "#c7c7c7";
+  const category2Color = categoryColors[item.category2] || "#c7c7c7";
+  const [createDate, setCreateDate] = useState(null);
+
+  useEffect(() => {
+    const jwtToken = sessionStorage.getItem("jwtAuthToken"); // 세션 스토리지에서 토큰 가져오기
+    const boardId = item.id;
+    if (jwtToken && boardId) {
+      axios
+        .get(`${HOST_URL}/api/v1/boards/${boardId}`, {
+          headers: { Authorization: `Bearer ${jwtToken}` },
+        })
+        .then((response) => {
+          setData(response.data);
+          if (response.status === 200) {
+            setTitle(response.data.data.title);
+            setContent(response.data.data.content);
+            setCategory1(response.data.data.category1);
+            setCategory2(response.data.data.category2);
+            const createAt = new Date(response.data.data.createAt);
+            const month = createAt.getMonth() + 1; // getMonth()는 0부터 시작하므로 1을 더해줍니다.
+            const day = createAt.getDate();
+            setCreateDate(`${month}월 ${day}일`);
+            console.log(response.data);
+          }
+        })
+        .catch((error) => {
+          console.log("게시글을 불러오는데 실패했습니다.");
+        });
+    }
+  }, [item]);
 
   return (
     <div className="flex w-full h-full px-3">
       <div className="w-full h-full ">
-        <div className="flex flex-col bg-white bg-opacity-75 rounded-[10px] gap-[10px]">
-          <div className="flex justify-between items-center px-[10px] mt-[10px]">
-            <div className="flex justify-center">
-              <Category category={category1} />
-              <Category category={category2} />
+        <div className="flex flex-col bg-white bg-opacity-75 rounded-[10px] ">
+          <div className="flex justify-between items-center px-[23px] my-[10px]">
+            <div className="flex">
+              <div className="w-[69px] h-[22px] bg-slate-400 rounded-[20px] mr-[3px]">
+                <div className="leading-[23px] text-center text-white text-[13px] font-semibold">
+                  {category1 || "Loading..."}
+                </div>
+              </div>
+              <div className="w-[69px] h-[22px] bg-slate-400 rounded-[20px]">
+                <div className="leading-[23px] text-center text-white text-[13px] font-semibold">
+                  {category2 || "Loading..."}
+                </div>
+              </div>
             </div>
-            <div className="flex justify-between items-center gap-[10px]">
+            <div className="flex justify-between gap-[10px]">
               <div className="flex justify-center text-center text-neutral-400 text-[15px] font-normal">
-                {/* {createAt || "Loading..."} */}
-                {`${month}월 ${day}일`}
+                {createDate || "Loading..."}
               </div>
               <div className="flex justify-center text-center text-neutral-400 text-[15px] font-normal">
                 <img
@@ -39,26 +80,27 @@ const VideoItem = ({
               </div>
             </div>
           </div>
-          <div className="flex justify-center">
+          <div className="flex">
             <img
-              className="mx-[40px] rounded-[10px] h-full w-[95%]"
-              src={thumbnailUrl}
+              className="px-[20px] m-0 rounded-[10px]"
+              src="https://voilio.s3.ap-northeast-2.amazonaws.com/thumbnail/test8.png"
               alt="thumbnail"
             />
           </div>
-          <div className="flex items-center pl-[20px] pb-[10px]">
+          <div className="flex justify-start items-center px-[20px] py-[10px]">
             <img
-              className="w-[60px] h-[60px] rounded-full m-0 object-cover"
-              src={imageUrl}
+              className="w-[60px] h-[60px] rounded-full m-0"
+              src="/asset/sample.png"
               alt="profile"
             />
-            <div className="flex flex-col ml-[15px] justify-center">
-              <div className="text-black text-[20px] font-semibol truncate overflow-ellipsis max-w-[45%]">
+            <div className="flex flex-col ml-[15px]">
+              <div className="text-black text-[20px] line-clamp-1 font-semibol">
                 {title || "Loading..."}
               </div>
-              <div className="text-neutral-700 text-[17px] font-normal truncate overflow-ellipsis max-w-[53%]">
-                {content || "Loading..."}
-              </div>
+              <div
+                className="text-neutral-700 line-clamp-1 text-[17px] font-normal"
+                dangerouslySetInnerHTML={{ __html: content }}
+              />
             </div>
           </div>
         </div>
--- src/component/ new-portal/VideoList.jsx
+++ src/component/ new-portal/VideoList.jsx
@@ -3,11 +3,8 @@ import VideoItem from "./VideoItem";
 import axios from "axios";
 import { HOST_URL } from "../../lib/HostUrl";
 
-const VideoList = ({ user_id }) => {
-  const [imageUrl, setimageUrl] = useState(null);
-  const [data, setData] = useState(null);
+const VideoList = () => {
   const [items, setItems] = useState([]);
-  const [createAt, setCreateAt] = useState(null);
 
   useEffect(() => {
     const jwtToken = sessionStorage.getItem("jwtAuthToken");
@@ -20,11 +17,6 @@ const VideoList = ({ user_id }) => {
         .then((response) => {
           if (response.status === 200) {
             setItems(response.data.data.content);
-            const createAt = new Date(response.data.data.createAt);
-            const month = createAt.getMonth() + 1; // getMonth()는 0부터 시작하므로 1을 더해줍니다.
-            const day = createAt.getDate();
-            setCreateAt(`${month}월 ${day}일`);
-            console.log("게시글을 정상적으로 불러왔습니다.");
           }
         })
         .catch((error) => {
@@ -33,49 +25,17 @@ const VideoList = ({ user_id }) => {
     }
   }, []);
 
-  useEffect(() => {
-    const jwtToken = sessionStorage.getItem("jwtAuthToken");
-
-    if (jwtToken) {
-      axios
-        .get(`${HOST_URL}/api/v1/auth/me`, {
-          headers: { Authorization: `Bearer ${jwtToken}` },
-        })
-        .then((response) => {
-          setData(response.data);
-          if (response.status === 200) {
-            setimageUrl(response.data.data.imageUrl);
-            console.log("프로필을 정상적으로 불러왔습니다.");
-          }
-        })
-        .catch((error) => {
-          console.log("프로필을 불러오는데 실패했습니다.");
-        });
-    }
-  }, []);
-
   return (
     <div className="w-full h-full">
-      <div className="grid grid-cols-3 grid-rows-3 gap-4 pl-[20px] pr-[70px]">
+      <div className="grid grid-cols-3 grid-rows-3 gap-4 gap-4 pl-[20px] pr-[55px]">
         {items.map((item) => (
-          <div
-            key={item.id}
-            className="bg-white bg-opacity-75 rounded-[10px] gap-[10px]"
-          >
-            <VideoItem
-              title={item.title}
-              content={item.content}
-              category1={item.category1}
-              category2={item.category2}
-              createAt={item.createAt}
-              imageUrl={imageUrl}
-              thumbnailUrl={item.thumbnailUrl}
-              user_id={item.id}
-            />
+          <div key={item.id} className="bg-white bg-opacity-75 rounded-[10px]">
+            <VideoItem item={item} /> {/* boardId를 props로 전달합니다. */}
           </div>
         ))}
       </div>
     </div>
+    // <VideoList></VideoList>
   );
 };
 
