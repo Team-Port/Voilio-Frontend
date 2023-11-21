d311157cb31ed160a53739b87342d46ba8e38dd5
Xoeon <jayy_19@ewhain.net>
2023-11-21 22:27:29 +0900

    [#83]Refactor: upload boards

--- src/modules/apis/axios-utils.js
+++ src/modules/apis/axios-utils.js
@@ -3,6 +3,7 @@ export const handleAxiosError = (error) => {
   console.error("Status Code:", error.response.status);
 
   if (error.response.status === 401) {
+    alert("로그인이 필요합니다. 로그인 창으로 이동됩니다.");
     window.location.href = "/new-portal/login";
   }
 };
